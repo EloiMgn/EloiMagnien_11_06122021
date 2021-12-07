3e4e75341ddc15e79c57fa50ac1dd1fac5de72e6
Eloi Magnien <eloi.magnien@gmail.com>
2021-12-07 19:53:28 +0100

    mise en page Gallery

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../Assets/LOGOW.svg'
+import './Footer.css'
+
+class Gallery extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+         <Link to="/"><img src={Logo} className="footer-logo" alt="Logo Kasa" /></Link>
+         <p className="footer_credits">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    )
+  }
+}
+
+export default Gallery 
