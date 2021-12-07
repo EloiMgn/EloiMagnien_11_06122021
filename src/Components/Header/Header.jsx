2b6f5a6f82e5d2d0632ae7b40b616d5e7b490b4d
Eloi Magnien <eloi.magnien@gmail.com>
2021-12-07 11:09:53 +0100

    mise en forme footer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../Assets/LOGO.svg'
+import Logo from '../../Assets/LOGOR.svg'
 import './Header.css'
 
 class Header extends React.Component {
