bd53e883449e895d33f7d5a9d0eee5d9d7b92ace
Eloi Magnien <eloi.magnien@gmail.com>
2021-12-08 17:55:59 +0100

    implémentation dropdown about + config sass

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import aboutBanner from '../../Assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.jpg';
-// import Header from '../../Components/Header/Header';
-// import Footer from '../../Components/Footer/Footer';
-
+import Dropdown from '../../Components/Dropdown/Dropdown';
+import './About.css';
 
 class Home extends React.Component {
-	render() {
-		return (
-			<div className='homepage'>
-				<Banner src={aboutBanner}/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="about">
+        <Banner src={aboutBanner} />
+        <div className="about__content">
+          <Dropdown
+            title="Fiabilité"
+            text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          />
+          <Dropdown
+            title="Respect"
+            text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          />
+          <Dropdown
+            title="Service"
+            text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+          />
+          <Dropdown
+            title="Sécurité"
+            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home; 
+export default Home;
