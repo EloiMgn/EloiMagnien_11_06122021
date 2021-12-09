5781efe48f1f302c5af21762ab8d3ba3c8617d8b
Eloi Magnien <eloi.magnien@gmail.com>
2021-12-09 11:04:50 +0100

    création dossiers composants fiche logement

---
+++ src/Pages/Accomodation/Accomodation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Accomodation.scss';
+import datas from '../../Datas/datas.json';
+
+class Accomodation extends React.Component {
+  render() {
+    return (
+      <div>
+        <i className="fas fa-chevron-left"></i>;
+        <img src="" alt="" />
+        <i className="fas fa-chevron-right"></i>;
+      </div>
+    );
+  }
+}
+
+export default Accomodation;
