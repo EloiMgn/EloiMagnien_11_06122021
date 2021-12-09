b151d49639717b9cb70f65964d1f68e134acdae4
Eloi Magnien <eloi.magnien@gmail.com>
2021-12-09 10:24:11 +0100

    création page Error 404 + routing error

--- src/index.jsx
+++ src/index.jsx
@@ -7,21 +7,21 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-
-
+import Error from './Components/Error/Error';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router className="index">
-     <Header />
-     <div className="mainDiv">
+      <Header />
+      <div className="mainDiv">
         <Routes>
           <Route excact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
         </Routes>
-     </div>
-      <Footer /> 
-    </Router> 
+      </div>
+      <Footer />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
